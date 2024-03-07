20ec6a1d7b60a9c7f9606e5161d8750db5622ca0
Carlos-SSH <carlos.anezeh@me.com>
2024-03-07 15:33:40 -0600

    Add create method to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
     @comments = @article.comments
