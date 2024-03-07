06e57b744dda446611dd5da157ed6d13d4905159
Carlos-SSH <carlos.anezeh@me.com>
2024-03-07 11:29:23 -0600

    Add basic rails project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comments = @article.comments
+    @comment = @article.comments.build
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :active, comments: [])
+  end
+end
